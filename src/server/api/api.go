b3e75dc69f2f0b804b14f90edbd686bcfbc2c0b3
Guillaume <Guillaume>
2024-03-12 15:17:44 +0100

    Added api

---
+++ src/server/api/api.go
@@ -0,0 +1,38 @@
+package api
+
+import (
+	"encoding/json"
+	"errors"
+	_ "log"
+	"net/http"
+)
+
+type ApiHandler struct {
+}
+
+func (handler ApiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var body = make(map[string]interface{})
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		jsonError(w, r, errors.New("Bad request"))
+		return
+	}
+
+	action, ok := body["action"]
+	if !ok {
+		jsonError(w, r, errors.New("Bad request: no action parameter"))
+		return
+	}
+
+	switch action {
+	case "get-countries":
+		err = getCountries(w, r)
+	default:
+		jsonError(w, r, NotFoundError{})
+		return
+	}
+
+	if err != nil {
+		jsonError(w, r, err)
+	}
+}
---
+++ src/server/api/printful.go
@@ -0,0 +1,45 @@
+package api
+
+import (
+	"crypto/tls"
+	"encoding/json"
+	"errors"
+	"github.com/baldurstod/printful-api-model"
+	"log"
+	"net/http"
+	"net/url"
+	"shop.loadout.tf/src/server/config"
+)
+
+var printfulConfig config.Printful
+
+func SetPrintfulConfig(config config.Printful) {
+	printfulConfig = config
+	log.Println(config)
+}
+
+func getCountries(w http.ResponseWriter, r *http.Request) error {
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+
+	u, err := url.JoinPath(printfulConfig.Endpoint, "/countries")
+	if err != nil {
+		return errors.New("Error while getting printful url")
+	}
+
+	resp, err := http.Get(u)
+
+	if err != nil {
+		log.Println(err)
+		return errors.New("Error while calling printful api")
+	}
+
+	countriesResponse := model.CountriesResponse{}
+	err = json.NewDecoder(resp.Body).Decode(&countriesResponse)
+	if err != nil {
+		return errors.New("Error while decoding printful response")
+	}
+
+	jsonSuccess(w, r, countriesResponse.Countries)
+
+	return nil
+}
