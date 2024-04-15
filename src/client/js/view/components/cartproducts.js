2f1432554d3f8681897591f7972d7f1a73416b18
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-04-15 23:01:37 +0200

    Added cart-products

---
+++ src/client/js/view/components/cartproducts.js
@@ -0,0 +1,30 @@
+import { createElement, I18n } from 'harmony-ui';
+
+export class CartProducts extends HTMLElement {
+
+	#refreshHTML(cart) {
+		this.innerHTML = '';
+
+		if (cart.totalQuantity > 0) {
+			for (let [productID, quantity] of cart.items) {
+				this.append(createElement('cart-item', {
+					elementCreated: element => element.setItem(productID, quantity, cart.currency),
+				}));
+			}
+		} else {
+			this.append(createElement('div', {
+				i18n: '#empty_cart',
+			}));
+		}
+
+		I18n.updateElement(this);
+	}
+
+	setCart(cart) {
+		this.#refreshHTML(cart);
+	}
+}
+
+if (window.customElements) {
+	customElements.define('cart-products', CartProducts);
+}
