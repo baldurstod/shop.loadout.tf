c3a1f0b0b7dc88be955989594dd1d24d286d7584
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-04-15 15:50:05 +0200

    Added variant

---
+++ src/server/model/variant.go
@@ -0,0 +1,23 @@
+package model
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Variant struct {
+	ID           primitive.ObjectID `json:"id" bson:"_id"`
+	Name         string             `json:"name" bson:"name"`
+	ThumbnailURL string             `json:"thumbnail_url" bson:"thumbnail_url"`
+	RetailPrice  float64            `json:"retail_price" bson:"retail_price"`
+	Currency     string             `json:"currency" bson:"currency"`
+	Options      []Option           `json:"options" bson:"options"`
+}
+
+func NewVariant(product *Product) Variant {
+	return Variant{
+		ID:           product.ID,
+		Name:         product.Name,
+		ThumbnailURL: product.ThumbnailURL,
+		RetailPrice:  product.RetailPrice,
+		Currency:     product.Currency,
+		Options:      product.Options,
+	}
+}
