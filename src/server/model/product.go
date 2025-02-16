9ca7dd8df326419a045be82fb8da82306b55b34f
Guillaume <108530065+baldurstod@users.noreply.github.com>
2025-02-16 22:20:18 +0100

    Refined product creation

--- src/server/model/product.go
+++ src/server/model/product.go
@@ -1,28 +1,26 @@
 package model
 
 import (
-	"github.com/greatcloak/decimal"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Product struct {
-	ID                primitive.ObjectID `json:"id" bson:"_id"`
-	Name              string             `json:"name" bson:"name"`
-	ProductName       string             `json:"product_name" bson:"product_name"`
-	ThumbnailURL      string             `json:"thumbnail_url" bson:"thumbnail_url"`
-	Description       string             `json:"description" bson:"description"`
-	IsIgnored         bool               `json:"is_ignored" bson:"is_ignored"`
-	DateCreated       int64              `json:"date_created" bson:"date_created"`
-	DateUpdated       int64              `json:"date_updated" bson:"date_updated"`
-	RetailPrice       decimal.Decimal    `json:"retail_price" bson:"retail_price"`
-	Currency          string             `json:"currency" bson:"currency"`
-	Files             []File             `json:"files" bson:"files"`
-	VariantIDs        []string           `json:"variant_ids" bson:"variant_ids"`
-	ExternalVariantID int64              `json:"external_variant_id" bson:"external_variant_id"`
-	HasMockupPictures bool               `json:"has_mockup_pictures" bson:"has_mockup_pictures"`
-	Options           []Option           `json:"options" bson:"options"`
-	Variants          []Variant          `json:"variants" bson:"variants"`
-	Status            string             `json:"status" bson:"status"`
+	ID           primitive.ObjectID `json:"id" bson:"_id"`
+	Name         string             `json:"name" bson:"name"`
+	ProductName  string             `json:"product_name" bson:"product_name"`
+	ThumbnailURL string             `json:"thumbnail_url" bson:"thumbnail_url"`
+	Description  string             `json:"description" bson:"description"`
+	IsIgnored    bool               `json:"is_ignored" bson:"is_ignored"`
+	DateCreated  int64              `json:"date_created" bson:"date_created"`
+	DateUpdated  int64              `json:"date_updated" bson:"date_updated"`
+	Files        []File             `json:"files" bson:"files"`
+	VariantIDs   []string           `json:"variant_ids" bson:"variant_ids"`
+	ExternalID1  string             `json:"external_id_1" bson:"external_id_1"`
+	ExternalID2  string             `json:"external_id_2" bson:"external_id_2"`
+	CustomData   map[string]any     `json:"custom_data" bson:"custom_data"`
+	Options      []Option           `json:"options" bson:"options"`
+	Variants     []Variant          `json:"variants" bson:"variants"`
+	Status       string             `json:"status" bson:"status"`
 }
 
 func NewProduct() Product {
--- src/server/model/requests/createproduct.go
+++ src/server/model/requests/createproduct.go
@@ -1,19 +1,14 @@
 package requests
 
 import (
-	"encoding/base64"
 	"errors"
-	"image"
-	"image/png"
-	"strings"
 )
 
 type CreateProductRequest struct {
-	VariantID    int    `mapstructure:"variant_id"`
-	Name         string `mapstructure:"name"`
-	Type         string `mapstructure:"type"`
-	Image        string `mapstructure:"image"`
-	DecodedImage image.Image
+	VariantID  int      `json:"variant_id"`
+	Name       string   `json:"name"`
+	Technique  string   `json:"technique"`
+	Placements []string `json:"placements"`
 }
 
 func (request CreateProductRequest) CheckParams() error {
@@ -24,22 +19,19 @@ func (request CreateProductRequest) CheckParams() error {
 	if request.Name == "" {
 		return errors.New("Invalid name")
 	}
+	/*
+		if request.Image == "" {
+			return errors.New("Invalid image")
+		}
 
-	if request.Type == "" {
-		return errors.New("Invalid type")
-	}
+		b64data := request.Image[strings.IndexByte(request.Image, ',')+1:] // Remove data:image/png;base64,
+		img, err := png.Decode(base64.NewDecoder(base64.StdEncoding, strings.NewReader(b64data)))
+		if err != nil {
+			return errors.New("Invalid image")
+		}
 
-	if request.Image == "" {
-		return errors.New("Invalid image")
-	}
-
-	b64data := request.Image[strings.IndexByte(request.Image, ',')+1:] // Remove data:image/png;base64,
-	img, err := png.Decode(base64.NewDecoder(base64.StdEncoding, strings.NewReader(b64data)))
-	if err != nil {
-		return errors.New("Invalid image")
-	}
-
-	request.DecodedImage = img
+		request.DecodedImage = img
+	*/
 
 	return nil
 }
