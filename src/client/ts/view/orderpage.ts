6ff15a6daded8243942cb509559b2677eaaaf084
Guillaume <108530065+baldurstod@users.noreply.github.com>
2025-03-12 22:28:22 +0100

    Fixed order summary page

--- src/client/ts/view/orderpage.ts
+++ src/client/ts/view/orderpage.ts
@@ -3,11 +3,17 @@ import orderPageCSS from '../../css/orderpage.css';
 import { Order } from '../model/order';
 import { ShopElement } from './shopelement';
 import { OrderItem } from '../model/orderitem';
-import { formatPrice } from '../utils';
+import { formatPercent, formatPrice } from '../utils';
 
 export class OrderPage extends ShopElement {
 	#order?: Order;
 	#htmlProducts?: HTMLElement;
+	#htmlPriceBreakDown?: HTMLElement;
+	#htmlItemsPrice?: HTMLElement;
+	#htmlTaxRate?: HTMLElement;
+	#htmlTaxPrice?: HTMLElement;
+	#htmlShippingPrice?: HTMLElement;
+	#htmlTotalPrice?: HTMLElement;
 
 	initHTML() {
 		if (this.shadowRoot) {
@@ -16,7 +22,47 @@ export class OrderPage extends ShopElement {
 		this.shadowRoot = createShadowRoot('section', {
 			adoptStyle: orderPageCSS,
 			childs: [
-				this.#htmlProducts = createElement('div', { class: 'products' }),
+				createElement('div', { class: 'title', i18n: '#order_summary' }),
+				createElement('div', {
+					class: 'summary',
+					childs: [
+						this.#htmlProducts = createElement('div', { class: 'items' }),
+						this.#htmlPriceBreakDown = createElement('div', {
+							class: 'price-breakdown',
+							childs: [
+								createElement('div', {
+									class: 'price-line',
+									childs: [
+										createElement('div', { class: "price-label", i18n: "#subtotal" }),
+										this.#htmlItemsPrice = createElement('div', { class: "price" }),
+									]
+								}),
+								createElement('div', {
+									class: 'price-line',
+									childs: [
+										createElement('div', { class: "price-label", i18n: "#shipping" }),
+										this.#htmlShippingPrice = createElement('div', { class: "price" }),
+									]
+								}),
+								createElement('div', {
+									class: 'price-line',
+									childs: [
+										createElement('div', { class: "price-label", i18n: "#tax" }),
+										this.#htmlTaxRate = createElement('div'),
+										this.#htmlTaxPrice = createElement('div', { class: "price" }),
+									]
+								}),
+								createElement('div', {
+									class: 'price-line',
+									childs: [
+										createElement('div', { class: "price-label", i18n: "#total" }),
+										this.#htmlTotalPrice = createElement('div', { class: "price" }),
+									]
+								}),
+							],
+						}),
+					],
+				}),
 			],
 		});
 		I18n.observeElement(this.shadowRoot);
@@ -32,6 +78,27 @@ export class OrderPage extends ShopElement {
 		this.#order.items.forEach((item) => {
 			this.#htmlProducts!.append(this.#htmlItemSummary(item, this.#order!.currency));
 		});
+
+
+		this.#htmlItemsPrice!.innerHTML = formatPrice(this.#order.itemsPrice, this.#order.currency);
+
+		this.#htmlTaxRate!.innerText = '';
+		if (this.#order.taxInfo) {
+			this.#htmlTaxRate!.innerText = ` (${formatPercent(this.#order.taxInfo.rate)})`;
+		}
+
+		if (this.#order.taxPrice) {
+			this.#htmlTaxPrice!.innerText = formatPrice(this.#order.taxPrice, this.#order.currency);
+		}
+
+		if (this.#order.shippingPrice) {
+			this.#htmlShippingPrice!.innerText = formatPrice(this.#order.shippingPrice, this.#order.currency);
+		}
+
+		if (this.#order.totalPrice) {
+			this.#htmlTotalPrice!.innerText = formatPrice(this.#order.totalPrice, this.#order.currency);
+		}
+
 	}
 
 	#htmlItemSummary(item: OrderItem, currency: string) {
@@ -40,8 +107,8 @@ export class OrderPage extends ShopElement {
 			childs: [
 				createElement('img', { class: 'thumb', src: item.getThumbnailUrl() }),
 				createElement('div', { class: 'name', innerText: item.getName() }),
-				createElement('div', { class: 'price', innerText: formatPrice(item.getRetailPrice(), currency) }),
 				createElement('div', { class: 'quantity', innerText: item.getQuantity() }),
+				createElement('div', { class: 'price', innerText: formatPrice(item.getRetailPrice(), currency) }),
 			]
 		});
 	}
