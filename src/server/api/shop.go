c2a50120fc560de8468ca16bc653a839fc607c34
Guillaume <108530065+baldurstod@users.noreply.github.com>
2025-04-17 00:18:59 +0200

    Added favorites.go

--- src/server/api/shop.go
+++ src/server/api/shop.go
@@ -29,22 +29,6 @@ func apiGetCurrency(c *gin.Context, s sessions.Session) error {
 	return nil
 }
 
-func apiGetFavorites(c *gin.Context, s sessions.Session) error {
-	favorites, ok := s.Get("favorites").(map[string]any)
-	if !ok {
-		favorites = make(map[string]any)
-	}
-
-	v := make([]string, 0, len(favorites))
-
-	for key := range favorites {
-		v = append(v, key)
-	}
-
-	jsonSuccess(c, map[string]any{"favorites": v})
-	return nil
-}
-
 type ProductPrice struct {
 	Currency string            `json:"currency" bson:"currency" mapstructure:"currency"`
 	Prices   map[string]string `json:"prices" bson:"prices" mapstructure:"prices"`
@@ -172,38 +156,6 @@ func apiSendMessage(c *gin.Context, params map[string]any) error {
 	}
 
 	jsonSuccess(c, map[string]any{"message_id": id})
-	return nil
-}
-
-func apiSetFavorite(c *gin.Context, s sessions.Session, params map[string]any) error {
-	if params == nil {
-		return errors.New("no params provided")
-	}
-
-	productId, ok := params["product_id"].(string)
-	if !ok {
-		return errors.New("missing params product_id")
-	}
-
-	isFavorite, ok := params["is_favorite"].(bool)
-	if !ok {
-		return errors.New("missing params is_favorite")
-	}
-
-	favorites, ok := s.Get("favorites").(map[string]any)
-	if !ok {
-		return errors.New("favorites not found")
-	}
-
-	if isFavorite {
-		favorites[productId] = struct{}{}
-	} else {
-		delete(favorites, productId)
-	}
-
-	log.Println(favorites)
-
-	jsonSuccess(c, nil)
 	return nil
 }
 
